3521712b44a48d418fad434849efbddc8a0e13d6
Sundari-99 <huntingknowl@gmail.com>
2021-07-26 21:27:18 +0530

    status changes - sabari

--- node/controllers/auth.js
+++ node/controllers/auth.js
@@ -112,7 +112,7 @@ exports.sign_in = async (req, res, next) => {
     var requests = req.bodyParams
     var page_status = 0;
     if (requests.email && requests.email != "") {
-        var user_detail = await User.findOne({ email: requests.email }).populate(['store_detail', 'category_id','driver_status_detail']);
+        var user_detail = await User.findOne({ email: requests.email,role: requests.role }).populate(['store_detail', 'category_id','driver_status_detail']);
     }
     else {
         var user_detail = await User.findOne({ role: requests.role, phone: requests.phone }).populate(['store_detail', 'category_id','driver_status_detail']);
