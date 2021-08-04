72fcded75f1833adcf05a68708d3c4cfbc7d5310
praveen-ak <43632875+praveen-ak@users.noreply.github.com>
2021-08-04 15:40:58 +0530

    is care giver added in trip detial

--- node/controllers/socket_controller.js
+++ node/controllers/socket_controller.js
@@ -15,7 +15,6 @@ module.exports.respond = function(socket_io){
       socket_io.join(room);
     });
     socket_io.on('update_location', async(data) => {
-      console.log(data)
       let location = {
         type: "Point",
         coordinates: [data.latitude,data.longitude]
