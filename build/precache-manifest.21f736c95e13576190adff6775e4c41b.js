e285074a5edc8a7b9b5d018cafb98b9149aaa043
praveen-ak <43632875+praveen-ak@users.noreply.github.com>
2021-08-12 13:48:09 +0530

    new build fixed user list in admin --ak

---
+++ build/precache-manifest.21f736c95e13576190adff6775e4c41b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ddc0fc928eda62125e4152c8997c8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc5066857207320911bb",
+    "url": "/static/css/7.535449d3.chunk.css"
+  },
+  {
+    "revision": "5b058f4e8b49c83b5eca",
+    "url": "/static/css/main.87e717f8.chunk.css"
+  },
+  {
+    "revision": "475a5b830115bfecfb32",
+    "url": "/static/js/0.f799048d.chunk.js"
+  },
+  {
+    "revision": "dea3175ad8f7953cd221",
+    "url": "/static/js/1.d303ae56.chunk.js"
+  },
+  {
+    "revision": "36037dc27da50365bed4",
+    "url": "/static/js/10.615d3ae0.chunk.js"
+  },
+  {
+    "revision": "8e725ba4cfbf57050ff3",
+    "url": "/static/js/11.95e5d5d6.chunk.js"
+  },
+  {
+    "revision": "729ff58ff1b8dfe33b93",
+    "url": "/static/js/12.72c3cdb4.chunk.js"
+  },
+  {
+    "revision": "1059354f564bd24ac964",
+    "url": "/static/js/2.a2c1f5bb.chunk.js"
+  },
+  {
+    "revision": "79ef178e56616a875c27",
+    "url": "/static/js/3.72b2bd75.chunk.js"
+  },
+  {
+    "revision": "6fdb140b04520609aaa8",
+    "url": "/static/js/4.f56ee638.chunk.js"
+  },
+  {
+    "revision": "dc5066857207320911bb",
+    "url": "/static/js/7.4f570bcf.chunk.js"
+  },
+  {
+    "revision": "4ca7333c4df121ae1e7f",
+    "url": "/static/js/8.9dd89737.chunk.js"
+  },
+  {
+    "revision": "198ca8c0d7cd3bb3efad",
+    "url": "/static/js/9.0a716cf4.chunk.js"
+  },
+  {
+    "revision": "5b058f4e8b49c83b5eca",
+    "url": "/static/js/main.c40e42f2.chunk.js"
+  },
+  {
+    "revision": "f35d48b141161efdc98f",
+    "url": "/static/js/runtime~main.cc5edaba.js"
+  },
+  {
+    "revision": "3e2074676586b45449dcf05cdca01ca6",
+    "url": "/static/media/bookLater.3e207467.png"
+  },
+  {
+    "revision": "05865d4f0f161de52f31c55efc757cdd",
+    "url": "/static/media/handyman.05865d4f.jpg"
+  },
+  {
+    "revision": "3f7f40b64b5ca89f288c22f8e10b1164",
+    "url": "/static/media/handyman2.3f7f40b6.jpg"
+  },
+  {
+    "revision": "906a6fbbb7e3d96fa103c01c2f476611",
+    "url": "/static/media/handyman3.906a6fbb.jpg"
+  },
+  {
+    "revision": "04ba26f8c19a70fc9e3e7f7cdc3539d2",
+    "url": "/static/media/logo.04ba26f8.png"
+  },
+  {
+    "revision": "cf173470f3d4490e5936219f2d1d589d",
+    "url": "/static/media/no_booking.cf173470.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d5fbeedde96c9e784a51792e31a30b0f",
+    "url": "/static/media/user_login.d5fbeedd.png"
+  }
+]);
--- build/static/js/10.560f092c.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{1702:function(e,n,a){"use strict";a.r(n),a.d(n,"default",function(){return s});var t=a(1),c=a.n(t),o=a(820),i=a.n(o),r=a(253);function s(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(r.a,{image:i.a,imageStyle:{height:200,objectFit:" scale-down"},description:c.a.createElement("span",{className:"bold"},"You have no bookings (Search Service)")}))}}}]);
-//# sourceMappingURL=10.560f092c.chunk.js.map
