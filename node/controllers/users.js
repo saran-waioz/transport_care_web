832299f8fe2d30b33125effd1f910461957f93af
praveen-ak <43632875+praveen-ak@users.noreply.github.com>
2021-08-28 20:21:30 +0530

    wallet data

--- node/controllers/users.js
+++ node/controllers/users.js
@@ -1439,30 +1439,29 @@ async function add_stripe_card_while_payment(requests,user_detail,trip_details) 
   }
   else if(requests.save_card && requests.card_type=="new")
   {
-    get_stripe_customer_id(user_detail).then(async(customer_id) => {
-      if(customer_id)
-      {
-        var card_details = await stripe.customers.createSource(customer_id,{
-          source: requests.token
-        })
-        if (!card_details.id) {
-          return {status:false,token:requests.token, type:"Error on saving stripe card"}
-        }
-        else {
-          var card_data = {}
-          card_data.card_id = card_details.id;
-          card_data.card_details = card_details;
-          card_data.user_id = requests.user_id;
-          let stripe_card_data = new StripeCards(card_data);
-          await stripe_card_data.save();
-          return {status:true,token:card_details.id, type:"saved_card"}
-        }
+    var customer_id = await get_stripe_customer_id(user_detail);
+    if(customer_id)
+    {
+      var card_details = await stripe.customers.createSource(customer_id,{
+        source: requests.token
+      })
+      if (!card_details.id) {
+        return {status:false,token:requests.token, type:"Error on saving stripe card"}
       }
-      else
-      {
-        return {status:false,token:requests.token, type:"Error on creating stripe customer"}
+      else {
+        var card_data = {}
+        card_data.card_id = card_details.id;
+        card_data.card_details = card_details;
+        card_data.user_id = requests.user_id;
+        let stripe_card_data = new StripeCards(card_data);
+        await stripe_card_data.save();
+        return {status:true,token:card_details.id, type:"saved_card"}
       }
-    })
+    }
+    else
+    {
+      return {status:false,token:requests.token, type:"Error on creating stripe customer"}
+    }
   }
   else
   {
