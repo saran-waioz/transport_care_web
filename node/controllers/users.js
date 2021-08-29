fbec3472d00fbf1961c4679a777235313782cae5
praveen-ak <43632875+praveen-ak@users.noreply.github.com>
2021-08-29 13:49:29 +0530

    wallet data

--- node/controllers/users.js
+++ node/controllers/users.js
@@ -1525,8 +1525,7 @@ async function make_payment(payment_data,trip_details,user_detail) {
   }
   else if(payment_mode=="wallet")
   {
-    console.log(parseFloat(user_detail.wallet_amount),parseFloat(payment_data.amount/100))
-    if(parseFloat(user_detail.wallet_amount)<=parseFloat(payment_data.amount/100))
+    if(parseFloat(payment_data.amount/100)<=parseFloat(user_detail.wallet_amount))
     {
       var updated_wallet_amount = (parseFloat(user_detail.wallet_amount)- parseFloat(payment_data.amount/100)).toFixed(2)
       await User.findOneAndUpdate(
