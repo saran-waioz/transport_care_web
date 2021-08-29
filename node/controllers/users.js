79fde6e4ebbfa8ceaaaacfe59b8db381d9b4b70d
praveen-ak <43632875+praveen-ak@users.noreply.github.com>
2021-08-29 14:12:33 +0530

    wallet data

--- node/controllers/users.js
+++ node/controllers/users.js
@@ -157,9 +157,9 @@ exports.get_user_detail = async (req, res) => {
     requests.id != "null"
   ) 
   {
-    var user_detail = await User.findOne({ _id: requests.id }).populate(['driver_status_detail','category_detail']);
+    var user_detail = await User.findOne({ _id: requests.id }).populate(['driver_status_detail','category_detail','default_category_detail']);
     var category_list = await Category.find();
-    get_default_category(user_detail,category_list).then(async(category_det) => {
+    // get_default_category(user_detail,category_list).then(async(category_det) => {
       var user_detail = await User.findOne({ _id: requests.id }).populate(['driver_status_detail','category_detail','default_category_detail']);
       user_detail = JSON.parse(JSON.stringify(user_detail));
       user_detail.default_category_name = user_detail.default_category_detail.name
@@ -179,7 +179,7 @@ exports.get_user_detail = async (req, res) => {
         amount:trip_details.price_detail.total
       }
       return res.apiResponse(true, "Success", { user_detail,service_type,category_list,last_trip_detail });
-    })
+    // })
   } else {
     return res.apiResponse(false, "Success");
   }
