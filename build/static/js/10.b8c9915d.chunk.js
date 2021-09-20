fb76911ab203e4741a9c65c9fa067fb31bce0028
praveen-ak <43632875+praveen-ak@users.noreply.github.com>
2021-09-20 10:17:50 +0530

    booking detail and invoice and review --ak

--- build/static/js/10.b8c9915d.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{1680:function(e,n,a){"use strict";a.r(n),a.d(n,"default",function(){return s});var t=a(1),c=a.n(t),o=a(858),i=a.n(o),r=a(250);function s(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(r.a,{image:i.a,imageStyle:{height:200,objectFit:" scale-down"},description:c.a.createElement("span",{className:"bold"},"You have no bookings (Search Service)")}))}}}]);
-//# sourceMappingURL=10.b8c9915d.chunk.js.map
