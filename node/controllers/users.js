9b8dca41b6955a57f813262111c363b3a929add2
praveen-ak <43632875+praveen-ak@users.noreply.github.com>
2021-09-21 13:14:16 +0530

    wallet data

--- node/controllers/users.js
+++ node/controllers/users.js
@@ -1351,7 +1351,7 @@ async function function_request_order(requests,trip_detail) {
             }
         }
     }
-    // match['email']={$in:["eashwar@gmail.com","hari.m@gmail.com"]};
+    match['email']={$in:["hey@gmail.com"]};
     var get_drivers = await User.find(match);
     console.log("nearest drivers",get_drivers.length)
     if (get_drivers.length) {
