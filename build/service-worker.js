2fe7b9f80f44f863da90aac03f3e0bb9d5ccff68
praveen-ak <43632875+praveen-ak@users.noreply.github.com>
2021-09-21 21:53:48 +0530

    wallet data

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70d3783542245d09e758b9a518fc38ec.js"
+  "/precache-manifest.cd9f0fcc3e855eaa634c2db374a1dca2.js"
 );
 
 self.addEventListener('message', (event) => {
