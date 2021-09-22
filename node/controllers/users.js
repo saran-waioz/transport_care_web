248a2d218c427ce3ca6510a4a99494cd1aae1abb
praveen-ak <43632875+praveen-ak@users.noreply.github.com>
2021-09-22 12:35:57 +0530

    wallet data

--- node/controllers/users.js
+++ node/controllers/users.js
@@ -1401,6 +1401,7 @@ async function function_request_order(requests,trip_detail) {
         for (let i = 0; i < get_drivers.length; ++i) {
           var already_requested_driver = await RequestDetail.find({ 'driver_id': get_drivers[i]._id });
           var distance = geolib.getDistance({ latitude: get_drivers[i].location.coordinates[1], longitude: get_drivers[i].location.coordinates[0] }, { latitude: orgin[1], longitude: orgin[0] })
+          console.log("1404",distance,orgin)
             var km = "1";
             if (distance >= 1000) {
                 km = parseFloat(parseFloat(distance) / 1000).toFixed(2);
