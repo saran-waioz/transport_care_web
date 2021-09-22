16ac622e986b3cd6e5e6385f0ab76e4e0ae4564a
praveen-ak <43632875+praveen-ak@users.noreply.github.com>
2021-09-22 12:56:03 +0530

    chart --ak

---
+++ build/precache-manifest.b2337370bf401408a424cfbe78eb4e4a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52dfe95a874443a234486b95bbd0d9d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f59ab468d77b624cd592",
+    "url": "/static/css/3.9eeeee62.chunk.css"
+  },
+  {
+    "revision": "1c25b2040b4fa79cea85",
+    "url": "/static/css/main.61c10b6b.chunk.css"
+  },
+  {
+    "revision": "0321caf233dfc835558d",
+    "url": "/static/js/0.0f86e6b6.chunk.js"
+  },
+  {
+    "revision": "e4c5de18c833b6194bc7",
+    "url": "/static/js/10.e53b6c51.chunk.js"
+  },
+  {
+    "revision": "99c97283e1ce43a7c94a",
+    "url": "/static/js/11.5bf8120f.chunk.js"
+  },
+  {
+    "revision": "f59ab468d77b624cd592",
+    "url": "/static/js/3.79ad5153.chunk.js"
+  },
+  {
+    "revision": "1ba1985837c843e90d1c",
+    "url": "/static/js/4.5d13e352.chunk.js"
+  },
+  {
+    "revision": "f7897199587a2f194a87",
+    "url": "/static/js/5.998c5c5b.chunk.js"
+  },
+  {
+    "revision": "ed02141da93c2255c34a",
+    "url": "/static/js/6.c47baf21.chunk.js"
+  },
+  {
+    "revision": "bb5dc4b1fb5d100bca3a",
+    "url": "/static/js/7.8a07aec3.chunk.js"
+  },
+  {
+    "revision": "425f8743444a5d1f0e29",
+    "url": "/static/js/8.28844473.chunk.js"
+  },
+  {
+    "revision": "b4aba44ae414f7ba290f",
+    "url": "/static/js/9.9dc21f91.chunk.js"
+  },
+  {
+    "revision": "1c25b2040b4fa79cea85",
+    "url": "/static/js/main.1f6b285b.chunk.js"
+  },
+  {
+    "revision": "0f141c1168876f25bd7f",
+    "url": "/static/js/runtime~main.698d0182.js"
+  },
+  {
+    "revision": "3e2074676586b45449dcf05cdca01ca6",
+    "url": "/static/media/bookLater.3e207467.png"
+  },
+  {
+    "revision": "05865d4f0f161de52f31c55efc757cdd",
+    "url": "/static/media/handyman.05865d4f.jpg"
+  },
+  {
+    "revision": "3f7f40b64b5ca89f288c22f8e10b1164",
+    "url": "/static/media/handyman2.3f7f40b6.jpg"
+  },
+  {
+    "revision": "906a6fbbb7e3d96fa103c01c2f476611",
+    "url": "/static/media/handyman3.906a6fbb.jpg"
+  },
+  {
+    "revision": "04ba26f8c19a70fc9e3e7f7cdc3539d2",
+    "url": "/static/media/logo.04ba26f8.png"
+  },
+  {
+    "revision": "cf173470f3d4490e5936219f2d1d589d",
+    "url": "/static/media/no_booking.cf173470.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d5fbeedde96c9e784a51792e31a30b0f",
+    "url": "/static/media/user_login.d5fbeedd.png"
+  }
+]);
